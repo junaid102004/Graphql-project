5437ab8b2540d62bf008100806ba8440ffffd18c
junaid102004 <junaidkhan610049.com>
2025-11-16 12:10:23 +0530

    juniad branch work

--- src/components/JavaScript.jsx
+++ src/components/JavaScript.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const JavaScript = () => {
   return (
-    <div>JavaScript</div>
+    <div>JavaScript juniad yaha par aya </div>
   )
 }
 
---
+++ src/components/JuniadKhan.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JuniadKhan = () => {
+  return (
+    <div>JuniadKhan</div>
+  )
+}
+
+export default JuniadKhan
